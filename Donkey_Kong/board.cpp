d4f3946f2dfcd50696f0482ea7cf3e0525c1155c
Hyunte <davidsond999@gmail.com>
2024-11-25 14:04:48 +0200

    added main menu

--- Donkey_Kong/board.cpp
+++ Donkey_Kong/board.cpp
@@ -4,9 +4,19 @@
 #include "Board.h"
 
 void Board::reset() {
-	for (int i = 0; i < MAX_Y; i++) {
-		memcpy(currentBoard[i], originalBoard[i], MAX_X + 1);
+	int i;
+	std::cin >> i;
+	if (i == 1) {
+		for (int i = 0; i < MAX_Y; i++) {
+			memcpy(currentBoard[i], originalBoard[i], MAX_X + 1);
+		}
 	}
+	if (i == 2) {
+		for (int i = 0; i < MAX_Y; i++) {
+			memcpy(currentBoard[i], mainMenu[i], MAX_X + 1);
+		}
+	}
+	
 }
 
 void Board::print() const {
