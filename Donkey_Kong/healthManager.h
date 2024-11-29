4ac2e0c793dee360b6bb2faf497ee52177f016e5
Hyunte <davidsond999@gmail.com>
2024-11-29 20:53:51 +0200

    changed and added health

---
+++ Donkey_Kong/healthManager.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "utils.h"
+#define MAX_HEALTH 3
+
+class Health {
+	/*static constexpr int MAX_X = 4;
+	static constexpr int MAX_Y = 3;
+	static constexpr int MAX_Z = 3;*/
+	int healthPosX = 75;
+	int healthPosY = 2;
+	int health = MAX_HEALTH;
+	
+	/*const char* healthDisplay[MAX_Y][MAX_Z] = {
+		{" __ ",  
+		 " __|", 
+		 " __|"},
+		{" __ ",
+		 " __|",
+		 "|__ "},
+		{"  |",
+		 "  |",
+		 "  |",}
+	};
+	char currentHealthDisplay[MAX_Y][MAX_X + 1]; // +1 for null terminator*/
+	
+public:
+	void draw(int c)  const {
+		gotoxy(healthPosX, healthPosY);
+		std::cout << c;
+		std::cout.flush();
+	}
+	void resetHealth();
+	void lowerHealth();
+	void printHealth() const;
+	/*char getChar(int x, int y) const {
+		return currentBoard[y][x];
+	}*/
+
+};
